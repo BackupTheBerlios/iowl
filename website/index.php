5979e80161b0c6e1dfc6e058ebb2b2aadbafa33f
fresh <fresh>
2002-03-10 14:50:37 +0000

    new site

---
+++ website/index.php
@@ -0,0 +1,11 @@
+<?php
+include '/home/groups/iowl/htdocs/header.php';
+?>
+<h1>Welcome to iOwl.net</h1>
+<p> iOwl.net is a recommendation service for the World Wide Web. The web browsing agent provides - depending on the
+personal preferences and interests - hints on interesting web sites. The completely distributed <a href="/product/features.php">peer-to-peer</a>
+network architecture of iOwl.net guarantees personalized search results, ease of use and a <a href="/product/privacy.php">
+maximum of privacy</a>.</p>
+<?php
+include '/home/groups/iowl/htdocs/footer.php';
+?>
