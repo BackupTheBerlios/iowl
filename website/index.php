fdb2e01bb009db44046e63859187de38ca1b8202
abiessmann <abiessmann>
2002-03-14 20:26:42 +0000

    added Link for wiki-sites

--- website/index.php
+++ website/index.php
@@ -5,7 +5,9 @@ include '/home/groups/iowl/htdocs/header.html';
 <p> iOwl.net is a recommendation service for the World Wide Web. The web browsing agent provides - depending on the
 personal preferences and interests - hints on interesting web sites. The completely distributed <a href="/product/features.php">peer-to-peer</a>
 network architecture of iOwl.net guarantees personalized search results, ease of use and a <a href="/product/privacy.php">
-maximum of privacy</a>.</p>
+maximum of privacy</a>.
+<br>
+Some hints for installing iOwl.net on linux can be found on <a href="http://www.iowl.net/wiki/">"wiki-sites"</a>.</p>
 <?php
 include '/home/groups/iowl/htdocs/footer.html';
 ?>
