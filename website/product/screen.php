5895e8d4c33aaad97e785eb943c1ed54a9e6dc74
abiessmann <abiessmann>
2002-03-14 21:31:03 +0000

    added new screenshots

--- website/product/screen.php
+++ website/product/screen.php
@@ -7,10 +7,13 @@ include '/home/groups/iowl/htdocs/header.html';
   - <a href="privacy.php">Privacy issues</a></h2>
 
 <p>Here you can see the history-page:</p>
-<p><img src="/pix/screen_history.gif" width="568" height="520" align="middle"></p>
+<p><img src="/pix/iowl-start.jpg" width="568" height="520" align="middle"></p>
 <p>&nbsp;</p>
 <p>Recommendations look like this:</p>
-<p><img src="/pix/screen_recommendation.gif" width="568" height="520" align="middle"></p>
+<p><img src="/pix/iowl-recommendation.jpg" width="568" height="520" align="middle"></p>
+<p>&nbsp;</p>
+<p>Rules site is a list of your "surf-profile"</p>
+<p><img src="/pix/iowl-rules.jpg" witdh="568" height="520" align="middle"></p>
 <?php
 include '/home/groups/iowl/htdocs/footer.html';
 ?>
