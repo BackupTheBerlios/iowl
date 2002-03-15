c2726f62895a3014bab5bc5bceb13e1e96b17214
aharth <aharth>
2002-03-15 21:42:23 +0000

    changed index.html, added berlios logo

--- website/index.php
+++ website/index.php
@@ -6,8 +6,15 @@ include '/home/groups/iowl/htdocs/header.html';
 personal preferences and interests - hints on interesting web sites. The completely distributed <a href="/product/features.php">peer-to-peer</a>
 network architecture of iOwl.net guarantees personalized search results, ease of use and a <a href="/product/privacy.php">
 maximum of privacy</a>.
-<br>
-Some hints for installing iOwl.net on linux can be found on <a href="http://www.iowl.net/wiki/">"wiki-sites"</a>.</p>
+</p>
+<p>
+Some hints for installing iOwl.net on Linux (in German) can be found on our<a href="http://www.iowl.net/wiki/">Wiki</a>.</p>
+
+<p>
+<a href="http://developer.berlios.de">
+<img src="http://developer.berlios.de/sflogo.php?group_id=0&type=1" width="59" h
+eight="26" border="0" alt="BerliOS Logo" /></a>
+</p>
 <?php
 include '/home/groups/iowl/htdocs/footer.html';
 ?>
