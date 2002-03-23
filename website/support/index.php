91d2eb30b8f4190a728f654d3e161e4468392929
Saruman <Saruman>
2002-03-23 10:29:06 +0000

    Simplified support page.

--- website/support/index.php
+++ website/support/index.php
@@ -4,14 +4,16 @@ include '/home/groups/iowl/htdocs/header.html';
 <h1>Support</h1>
 <p>When using iOwl.net some questions might come up. The support
  section gives answers to common questions including software installation,
- configuration, usage, known bugs and so on. </p>
-<p>To get an answer to frequently asked questions please go to the section
- <a href="faq.php">FAQ</a>. The <a href="usermanual.php">user manual</a> describes
- the functions of iOwl.net and gives a howto part including installation
- instructions and hints on using the software.</p>
-<p>If there are still some questings please contact us by
- <a href="email.php"> eMail</a>. <br> For Bugreports you should use berlios bug-tracking
- <a href="http://developer.berlios.de/bugs/?func=addbug&group_id=250">iOwl.net Bug Tracking</a>. </p>
+ configuration and usage.</p>
+
+<ul type='disc'>
+ <li><a href="faq.php">Frequently Asked Questions</a></li>
+ <li><a href="usermanual.php">User Manual</a></li>
+ <li><a href="email.php">eMail Contact</a></li>
+</ul>
+
+<p>For Bugreports you should use the
+ <a href="http://developer.berlios.de/bugs/?func=addbug&group_id=250">bugtracker</a>. </p>
 <p>&nbsp;</p>
 <h2><a href="faq.php">FAQ</a> - <a href="usermanual.php">User Manual</a> - <a href="email.php">eMail</a></h2>
 <?php
