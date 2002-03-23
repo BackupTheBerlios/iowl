3bb0f1dccbf0506ab1084882976099203927af7d
Saruman <Saruman>
2002-03-23 10:29:34 +0000

    removed invalid mailto-links.

--- website/support/email.php
+++ website/support/email.php
@@ -7,12 +7,9 @@ include '/home/groups/iowl/htdocs/header.html';
   - <a href="usermanual.php">User Manual</a>
   - <a href="email.php">eMail</a>
 </h2>
-<p>Please read the <a href="faq.php">frequently asked questions</a>
-   first before you post an eMail. Many issues have already been
-   answered and the FAQ is updated permanently. </p>
-<p><b>Support:</b> <a href="mailto:support@iowl.net">support@iowl.net</a></p>
-<p><b>Bug reports:</b> <a href="mailto:bugs@iowl.net">bugs@iowl.net</a></p>
-<p><b>Mailing list:</b> <a href="mailto:iowl-users@lists.berlios.de">iowl-users@lists.berlios.de</a></p>
+<p>If you have any questions or comments, please contact us via the Mailing
+list: <a href="mailto:iowl-users@lists.berlios.de">iowl-users@lists.berlios.de</a></p>
+
 <?php
 include '/home/groups/iowl/htdocs/footer.html';
 ?>
